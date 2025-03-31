f3277829ee6ed3939379e961ac599b4661e3c58e
arnav <arnav.mathur.1024@gmail.com>
2025-03-31 19:09:09 +0530

    made a few changes to styling

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,8 +5,8 @@ const Navbar = (props)=>{
     const user = props.user;
     return(
         <div className="navbar">
-            <Link to='/'>Home</Link>
-            {user && <Link to='/login'>Login</Link>}
+            <Link to='/' className="home">Home</Link>
+            {!user && <Link to='/login' className="login">Login</Link>}
             {user || <p>{user}</p>}
         </div>
     )
