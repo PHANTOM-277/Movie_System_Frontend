5b86bd747bc6505cbf5161d12ec65d9dd33378c4
arnav <arnav.mathur.1024@gmail.com>
2025-03-31 19:10:34 +0530

    removed useless components from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,12 @@ import { Routes,Route, useParams } from 'react-router-dom'
 import Login from './pages/Login'
 import Home from './pages/Home'
 
-const Movies = ()=>{
-  return(<h1>Movies</h1>)
-}
-
-const MovieDetails = ()=>{
-  const {id} = useParams();
-  return(<h1>Movie number :  {id}</h1>)
-}
 
 function App() {
   return(
     <Routes>
       {/* routes which dont need login */}
       <Route path='/' element={<Home />} />
-      <Route path='/movies' element={<Movies />} />
       
       
       {/* registeration and login route */}
